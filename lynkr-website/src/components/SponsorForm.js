19d24a9efc5c5a227ac907291932a222ec82bf7d
mwcooper44 <maxwcoop@gmail.com>
2024-11-03 17:57:34 -0500

    updated multiple page

--- lynkr-website/src/components/SponsorForm.js
+++ lynkr-website/src/components/SponsorForm.js
@@ -12,12 +12,21 @@ function SponsorForm() {
     attendance: '',
     brandActivation: '',
     exclusions: '',
+    sizeOfEvent: '',
+    numberOfPeople: '',
+    amountOfProduct: '',
     shipping: '',
-    shippingDays: '',
+    shippingNoticeNumber: '',
+    shippingNoticePeriod: '',
     mediaStyle: '',
     photosRequired: '',
-    videosRequired: ''
+    videosRequired: '',
+    additionalMaterials: ''
   });
+  const [showPhotosDropdown, setShowPhotosDropdown] = useState(false);
+  const [showVideosDropdown, setShowVideosDropdown] = useState(false);
+  const [showMarketing, setShowMarketing] = useState(false);
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -28,20 +37,22 @@ function SponsorForm() {
   const handleFileChange = (e) => {
     setFormData({
       ...formData,
-      coverImage: e.target.files[0], // Store the uploaded file in formData
+      coverImage: e.target.files[0],
     });
   };
-  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const togglePhotosDropdown = () => setShowPhotosDropdown(!showPhotosDropdown);
+  const toggleVideosDropdown = () => setShowVideosDropdown(!showVideosDropdown);
+  const toggleAdditionalMarketingMaterials = () => setShowMarketing(!showMarketing);
+
   const nextStep = () => setCurrentStep((prev) => Math.min(prev + 1, 6));
   const prevStep = () => setCurrentStep((prev) => Math.max(prev - 1, 1));
 
-  // Validation for each step
   const isStepValid = () => {
     switch (currentStep) {
       case 1:
@@ -49,13 +60,13 @@ function SponsorForm() {
       case 2:
         return formData.brandActivation;
       case 3:
-        return true; // Optional fields in step 3, so always valid
+        return formData.exclusions;
       case 4:
-        return formData.shipping && formData.shippingDays;
+        return formData.sizeOfEvent;
       case 5:
-        return formData.mediaStyle;
+        return true; //optional
       case 6:
-        return formData.photosRequired && formData.videosRequired;
+        return formData.shippingNoticeNumber && formData.shippingNoticePeriod && (!showMarketing || (formData.additionalMaterials && showMarketing));
       default:
         return false;
     }
@@ -86,7 +97,7 @@ function SponsorForm() {
             <div className="form-group">
               <div>
                 <label htmlFor="brandActivation">What does your Ideal Brand Activation Look Like?</label>
-                <input type="text" name="brandActivation" placeholder="Describe your Ideal Brand Activation:" value={formData.brandActivation} onChange={handleChange} />
+                <input type="text" name="brandActivation" placeholder="Describe your Ideal Brand Activation:" value={formData.brandActivation} onChange={handleChange} required/>
               </div>
             </div>
           )}
@@ -94,44 +105,119 @@ function SponsorForm() {
             <div className="form-group">
               <div>
                 <label htmlFor="exclusions">Are there Any Organizations you don't want to Sponsor?</label>
-                <input type="text" name="exclusions" placeholder="ex: Greek Life, Professional, etc. N/A if none" value={formData.exclusions} onChange={handleChange} />
+                <input type="text" name="exclusions" placeholder="ex: Greek Life, Professional, etc. N/A if none" value={formData.exclusions} onChange={handleChange} required/>
               </div>
               
             </div>
           )}
           {currentStep === 4 && (
             <div className="form-group">
-              <div></div>
-              <input type="text" name="shipping" placeholder="Will you be shipping marketing materials?" value={formData.shipping} onChange={handleChange} required />
-              <select name="shippingDays" value={formData.shippingDays} onChange={handleChange} required>
-                <option value=""># days before to ship marketing materials</option>
-                <option value="1">1</option>
-                <option value="7+">7+</option>
-                <option value="14+">14+</option>
-              </select>
+              <div>
+                <label htmlFor="sizeOfEvent">Ideal Size of Event:</label>
+                <select name="sizeOfEvent" value={formData.sizeOfEvent} onChange={handleChange} required>
+                  <option value="">ideal # of people in attendance</option>
+                  <option value="0-25">0-25</option>
+                  <option value="25-50">25-50</option>
+                  <option value="50-100">50-100</option>
+                  <option value="100+">100+</option>
+                </select>
+              </div>
+              <div>
+                <label>How much Product will you send for X number of People?</label>
+                <label className="example">ex: We will send 2 cases of product for every 20 people in attendance.</label>
+                <label>For every X number of people,</label>
+                <input type="text" name="numberOfPeople" placeholder="5, 25, 50 people" value={formData.numberOfPeople} onChange={handleChange} required/>
+                
+              </div>
+              <div>
+                <label>we will send Y of Product.</label>
+                <input type="text" name="amountOfProduct" placeholder="please specify: 1 case, 5 boxes, 2 pallets, etc" value={formData.amountOfProduct} onChange={handleChange} required/>
+              </div>
+              
             </div>
           )}
           {currentStep === 5 && (
             <div className="form-group">
-              <input type="text" name="mediaStyle" placeholder="Describe the Style of Photos and Videos you Require:" value={formData.mediaStyle} onChange={handleChange} required />
+              <label>Content Requirements?</label>
+              <div>
+                <div className="checkbox-container">
+                  <label htmlFor="photosCheckbox">Photos</label>
+                  <input type="checkbox" id="photosCheckbox" className="custom-checkbox" onChange={togglePhotosDropdown} />
+                </div>
+                {showPhotosDropdown && (
+                  <select name="photosRequired" value={formData.photosRequired} onChange={handleChange}>
+                    <option value=""># photos required</option>
+                    <option value="1-3">1-3</option>
+                    <option value="3-5">3-5</option>
+                    <option value="5-10">5-10</option>
+                    <option value="10+">10+</option>
+                  </select>
+                )}
+              </div>
+              <div>
+              <div className="checkbox-container">
+                  <label htmlFor="videosCheckbox">Videos</label>
+                  <input type="checkbox" id="videosCheckbox" className="custom-checkbox" onChange={toggleVideosDropdown} />
+                </div>
+                {showVideosDropdown && (
+                  <select name="videosRequired" value={formData.videosRequired} onChange={handleChange}>
+                    <option value=""># videos required</option>
+                    <option value="1-3">1-3</option>
+                    <option value="3-5">3-5</option>
+                    <option value="5-10">5-10</option>
+                    <option value="10+">10+</option>
+                  </select>
+                )}
+              </div>
+              <div>
+                <label htmlFor="mediaStyle">(Optional): Describe any content preferences:</label>
+                <input
+                  type="text"
+                  id="mediaStyle"
+                  name="mediaStyle"
+                  placeholder="ex: logos facing camera, people engaged in activities"
+                  value={formData.mediaStyle}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
           )}
           {currentStep === 6 && (
             <div className="form-group">
-              <select name="photosRequired" value={formData.photosRequired} onChange={handleChange} required>
-                <option value=""># photos required</option>
-                <option value="1">1</option>
-                <option value="3">3</option>
-                <option value="5">5</option>
-                <option value="10+">10+</option>
-              </select>
-              <select name="videosRequired" value={formData.videosRequired} onChange={handleChange} required>
-                <option value=""># videos required</option>
-                <option value="1">1</option>
-                <option value="3">3</option>
-                <option value="5">5</option>
-                <option value="10+">10+</option>
-              </select>
+              <label htmlFor="shipping">Event Notice Required for Shipping Product:</label>
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }} className="shippingNotice">
+                <input
+                  type="text"
+                  id="shippingNoticeNumber"
+                  name="shippingNoticeNumber"
+                  placeholder="please enter a number:"
+                  value={formData.shippingNoticeNumber}
+                  onChange={handleChange}
+                />
+                <select name="shippingNoticePeriod" value={formData.shippingNoticePeriod} onChange={handleChange}>
+                    <option value="">period of time</option>
+                    <option value="day(s)">day(s)</option>
+                    <option value="week(s)">week(s)</option>
+                    <option value="month(s)">month(s)</option>
+                  </select>
+              </div>
+
+              <div>
+                <div className="checkbox-container">
+                  <label htmlFor="marketingCheckbox">Will you be providing Additional Marketing Materials?</label>
+                  <input type="checkbox" id="marketingCheckbox" className="custom-checkbox" onChange={toggleAdditionalMarketingMaterials} />
+                </div>
+                {showMarketing && (
+                  <input
+                  type="text"
+                  id="additionalMaterials"
+                  name="additionalMaterials"
+                  placeholder="ex: merch, banners, flags"
+                  value={formData.additionalMaterials}
+                  onChange={handleChange}
+                />
+                )}
+              </div>
             </div>
           )}
 
